ccebb702a40bc1e60a298e6334edabf024ade7a0
Expant <anton532202@gmail.com>
2024-10-21 17:01:05 +0300

    projects section

--- src/components/ContentItemsProjects.vue
+++ src/components/ContentItemsProjects.vue
@@ -1,8 +1,8 @@
 <script setup>
 import gsap from 'gsap';
 import ContentItemsProjectsItem from './ContentItemsProjectsItem.vue';
-// import { onMounted, reactive, ref } from 'vue';
-// import ScrollTrigger from 'gsap/src/ScrollTrigger';
+import { onMounted } from 'vue';
+import ScrollTrigger from 'gsap/src/ScrollTrigger';
 
 const projects = [
   { id: 0, title: 'RSS-Агрегатор' },
@@ -11,25 +11,30 @@ const projects = [
   { id: 3, title: 'Your tour' },
 ];
 
-// onMounted(() => {
-//   gsap.from('.animate .line', {
-//     y: 500,
-//     x: 1000,
-//     rotation: 90,
-//     opacity: 0,
-//     duration: 2,
-//     ease: 'Second.in',
-//     stagger: 0.1,
-//     scrollTrigger: {
-//       trigger: '.animate',
-//       start: 'top center',
-//       end: 'center center',
-//       markers: true,
-//       scrub: 2,
-//       toggleActions: 'restart pause reverse none',
-//     },
-//   });
-// });
+const showProjects = () => {
+  gsap.from('.animate .line', {
+    y: 500,
+    x: 1000,
+    rotation: 90,
+    opacity: 0,
+    duration: 2,
+    ease: 'Second.in',
+    stagger: 0.1,
+    scrollTrigger: {
+      trigger: '.animate',
+      start: 'top center',
+      end: 'center center',
+      markers: true,
+      scrub: 2,
+      toggleActions: 'restart pause reverse none',
+    },
+  });
+};
+
+onMounted(() => {
+  gsap.registerPlugin(ScrollTrigger);
+  showProjects();
+});
 
 </script>
 
@@ -37,8 +42,9 @@ const projects = [
   <section class="animate h-screen py-4 px-5 md:py-20 md:px-10">
     <ul class="h-full flex flex-col justify-center items-start gap-y-20">
       <content-items-projects-item
-        v-for="[id, title] in projects"
+        v-for="{id, title} in projects"
         :key="id"
+        :id="id"
       >
       {{ title }}
       </content-items-projects-item>
--- src/components/ContentItemsProjectsItem.vue
+++ src/components/ContentItemsProjectsItem.vue
@@ -1,25 +1,30 @@
 <script setup>
 import gsap from 'gsap';
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
+import ScrollTrigger from 'gsap/src/ScrollTrigger';
 
 const hoverProject = ref();
+const props = defineProps({
+  id: Number,
+})
 
 onMounted(() => {
-  hoverProject.value = gsap.to('.project', { paused: true, x: 20, duration: 0.2 });
+  hoverProject.value = gsap.to(`.project-${props.id}`, { paused: true, duration: 0.2 });
 });
 </script>
 
 <template>
   <li 
-    class="project relative"
+    :class="['project', 'relative']"
     @mouseover="hoverProject.play()"
     @mouseleave="hoverProject.reverse()"
   >
-    <a 
+    <a
       href="#"
       :class="[
-        'line',
-        'inline-block font-montserrat bg-yellow-400 text-lg p-3 uppercase bg-transparent', 
+        `project-${props.id} line`,
+        'inline-block font-montserrat text-lg p-3 uppercase bg-yellow-400 transition-all',
+        'hover:bg-[#333] hover:text-[#fff]',
         'md:text-4xl lg:text-5xl xl:text-7xl',
       ]" 
     >
